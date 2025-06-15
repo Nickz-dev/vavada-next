37b4432c3914052c47389559ca1792e5cac33182
Nickz-dev <nick.apios@gmail.com>
2025-06-15 23:15:12 +0700

    first commit

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 const generateSitemap = () => {
-  const pages = ['/', 'ru', '/en', '/bonuses', '/registration', '/mirrors',  '/slots','/payments', '/download'];
+  const pages = ['/', '/ru', '/en', '/bonuses', '/registration', '/mirrors',  '/slots','/payments', '/download'];
   
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
